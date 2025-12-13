54c10d53cf08dba03fdbe141833dd9a195c7629d
vaishnavi11-p <vaishnavi.22310198@viit.ac.in>
2025-12-13 21:36:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,13 @@ image = st.file_uploader("Upload room image", type=["jpg","png"])
 if image:
     st.image(image, caption="Original Room")
 
+room_type = st.selectbox(
+    "Select Room Type",
+    ["Bedroom", "Living Room", "Office"]
+)
+
+style = st.selectbox(
+    "Select Design Style",
+    ["Modern", "Minimalist", "Luxury"]
+)
+
