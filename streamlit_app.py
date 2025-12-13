7c9a59e76ebe7dd4a6b81d894ea4a32ac7dffdfc
vaishnavi11-p <vaishnavi.22310198@viit.ac.in>
2025-12-13 21:32:32 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("My First Streamlit App")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
